9b882f9a4613633cf8f5c44ffb6736ed7135d34b
dh365x <dh365x@gmail.com>
2023-11-10 16:58:41 +0900

    Setup, Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,56 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import Layout from './components/Layout';
+import Profile from './components/Profile';
+import Home from './components/Home';
+import Login from './components/Login';
+import CreateAccount from './components/CreateAccount';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: 'login',
+    element: <Login />,
+  },
+  {
+    path: 'create-account',
+    element: <CreateAccount />,
+  },
+]);
+
+const GloblaStyle = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color: white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
 function App() {
-  return <div />;
+  return (
+    <>
+      <GloblaStyle />
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
