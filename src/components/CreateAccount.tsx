01d287b188836a4e978c4fc320564f3bfb585c4f
dh365x <dh365x@gmail.com>
2023-11-14 18:29:53 +0900

    Authentication, Form State

--- src/components/CreateAccount.tsx
+++ src/components/CreateAccount.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -42,21 +43,73 @@ const Error = styled.span`
 `;
 
 export default function CreateAccount() {
+  const [isLoading, setLoading] = useState(false);
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === 'username') {
+      setUsername(value);
+    } else if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    try {
+      // 1. 계정 생성하기
+      // 2. 사용자 이름 설정하기
+      // 3. home 경로로 돌아가기
+    } catch (e) {
+      // 에러 발생 로직
+    } finally {
+      setLoading(false);
+      console.log(username, email, password);
+    }
+  };
+
   return (
     <Wrapper>
       <Title>Log into X</Title>
-      <Form>
-        <Input type="text" name="name" placeholder="Name" required />
-        <Input type="email" name="email" placeholder="Email" required />
+      <Form onSubmit={onSubmit}>
         <Input
+          value={username}
+          onChange={onChange}
+          type="text"
+          name="username"
+          placeholder="User Name"
+          required
+        />
+        <Input
+          value={email}
+          onChange={onChange}
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+        />
+        <Input
+          value={password}
+          onChange={onChange}
           type="password"
           name="password"
           placeholder="Password"
           required
         />
-        <Input type="submit" />
+        <Input
+          value={isLoading ? 'Loading...' : 'Create Account'}
+          type="submit"
+        />
       </Form>
-      <Error>Error Message</Error>
+      {error !== '' ? <Error>{error}</Error> : null}
     </Wrapper>
   );
 }
