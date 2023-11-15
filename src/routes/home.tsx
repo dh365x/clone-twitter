734903cf7e41faf9493ff9c3131417f02fd3aac2
dh365x <dh365x@gmail.com>
2023-11-15 22:23:52 +0900

    Tweeting, Navigation Bar Styling

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,13 +1,3 @@
-import { auth } from '../firebase';
-
 export default function Home() {
-  const logOut = () => {
-    auth.signOut();
-  };
-
-  return (
-    <button onClick={logOut} type="button">
-      Log Out
-    </button>
-  );
+  return <h1>Home</h1>;
 }
