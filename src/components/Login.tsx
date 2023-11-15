21e8c8763bde28ec3eef39e5a4474f16f582b4db
dh365x <dh365x@gmail.com>
2023-11-15 10:54:10 +0900

    Authentication, Style Refactoring

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,58 +1,9 @@
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import { Link, useNavigate } from 'react-router-dom';
 import { FirebaseError } from 'firebase/app';
 import { auth } from '../firebase';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  height: 100%;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-  margin-top: 50px;
-  margin-bottom: 15px;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 10px 20px;
-  border: none;
-  border-radius: 50px;
-  font-size: 16px;
-  &[type='submit'] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  text-align: center;
-  font-weight: 600;
-  color: tomato;
-`;
-
-const Switcher = styled.span`
-  margin-top: 20px;
-  a {
-    color: #1d96f0;
-  }
-`;
+import { Input, Title, Wrapper, Switcher, Error, Form } from './AuthComponents';
 
 export default function Login() {
   const [isLoading, setLoading] = useState(false);
